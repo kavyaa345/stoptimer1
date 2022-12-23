c5dc732207aca0ed856b678a3dade2ba429331a9
kavyaa345 <118717764+kavyaa345@users.noreply.github.com>
2022-12-23 18:27:42 +0530

    Add files via upload

---
+++ ic_play.java
@@ -0,0 +1,14 @@
+package com.example.stoptimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ic_play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ic_play);
+    }
+}
